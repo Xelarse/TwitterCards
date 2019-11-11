628f4943a7ad6cbf75b9086021fa960b2a59dbde
Alex Allman <xalexxd@hotmail.co.uk>
2019-11-11 23:29:09 +0000

    Laid out collection view and set up some classes it

---
+++ TwitterCards/View/Card Selection/CardSelectCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardSelectCollectionCell.swift
+//  TwitterCards
+//
+//  Created by Alex Allman on 11/11/2019.
+//  Copyright © 2019 Alex Allman. All rights reserved.
+//
+
+import UIKit
+
+class CardSelectCollectionCell : UICollectionViewCell{
+    
+}
